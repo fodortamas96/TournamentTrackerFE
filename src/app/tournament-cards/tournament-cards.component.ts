9377efbbdb006a5502efb8aef0ff24d27d26577f
Fodor <fodor.tamas48@gmail.com>
2024-05-15 22:27:21 +0200

    Created tournamentCards component and imported in the tournament

---
+++ src/app/tournament-cards/tournament-cards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tournament-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './tournament-cards.component.html',
+  styleUrl: './tournament-cards.component.css'
+})
+export class TournamentCardsComponent {
+
+}
--- src/app/tournaments/tournaments.component.ts
+++ src/app/tournaments/tournaments.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { TournamentCardsComponent } from '../tournament-cards/tournament-cards.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tournaments',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TournamentCardsComponent
+  ],
   templateUrl: './tournaments.component.html',
   styleUrl: './tournaments.component.css'
 })
 export class TournamentsComponent {
-
+  
 }
